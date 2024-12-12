7254fbe45e12a208bdebbd325c43484c78754810
Proteeti Kushari <60457670+ProteetiK@users.noreply.github.com>
2024-12-12 10:37:53 +0000

    Update Water.h

--- Components/Water/Water.h
+++ Components/Water/Water.h
@@ -1,27 +1,8 @@
 //GL imports
 #include <glm/glm.hpp>
 
-class Water {
-public:
-    //constructor
-    Water(float water_width, float water_height);
-    //initualizer
-    void init();
-    //rendering method
-    void render(const glm::mat4 viewMatrix, const glm::mat4 projectionMatrix, float y);
-
-private:
-    //dimensions of water overlay
-    float water_width;
-    float water_height;
-
-    //shader function
-    GLuint shaderProgramID_water;
-    GLuint water_vao, water_vbo;
-    GLuint textureMap;
-    //position variables
-    GLint water_loc_model, water_loc_view, water_loc_proj, water_loc_time;
-
-    //main implementation
-    GLuint loadWaterTexture();
-};
+//main rendering implementation
+void waterRender(const mat4 viewMatrix, const mat4 projectionMatrix, float y, int water_width, int water_height);
+   
+//load amd map water texture
+GLuint loadWaterTexture();
