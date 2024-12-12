a8bb08996df63008e414e8be6585eca678a2c303
Proteeti Kushari <60457670+ProteetiK@users.noreply.github.com>
2024-12-12 10:34:59 +0000

    Update LoadModel.h

--- Components/LoadModel.h
+++ Components/LoadModel.h
@@ -11,12 +11,16 @@
 
 #pragma once
 
+using namespace std;
+
+//define the basic model structure for the 3D mesh models loaded into the project
 typedef struct ModelData
 	{
 		size_t mPointCount = 0;
-		std::vector<vec3> mVertices;
-		std::vector<vec3> mNormals;
-		std::vector<vec2> mTextureCoords;
+		vector<vec3> mVertices;
+		vector<vec3> mNormals;
+		vector<vec2> mTextureCoords;
 	} ModelData;
 
+//load 3D models into the project
 ModelData loadModelMesh(static const char* meshName);
